dfcead3bb0b724341782e7a502567b3c5d043930
José Cássio <josecassio2013@gmail.com>
2023-04-06 15:10:06 -0300

    configure stitches css

---
+++ src/styles/index.ts
@@ -0,0 +1,25 @@
+import {createStitches} from '@stitches/react'
+
+export const {
+    config,
+    styled,
+    css,
+    globalCss,
+    keyframes,
+    getCssText,
+    theme,
+    createTheme
+} = createStitches({
+    theme: {
+        colors:{
+            purple: '#990099',
+            blue_1: '#00CFFF',
+            blue_2: '#82E7FF',
+            red: '#FF3600',
+            white_1: '#F0F0FC',
+            white_2: '#FFFFFF',
+            text: '#B2BFBA',
+            border: '#990099',
+        }
+    }
+})
