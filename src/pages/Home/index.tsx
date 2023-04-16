f79e8b556d327f2bfe8bf96127f38d079adc1dc4
José Cássio <josecassio2013@gmail.com>
2023-04-16 19:57:23 -0300

    add menu component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components/Header"
 
 import { Container } from './styles'
+import { Menu } from "@/components/Menu"
 
 export default function Home() {
   return (
     <Container>
      <Header/>
+     <Menu/>
     </Container>
   )
 }
