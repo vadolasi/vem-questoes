89dafe430de319502396bd8e386a26054e82fb6d
José Cássio <josecassio2013@gmail.com>
2023-04-16 19:56:41 -0300

    add grid template

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,4 +3,13 @@ import { styled } from '../../styles'
 export const Container = styled('div', {
         width: "100vw",
         height: "100vh",
-})
+
+        display: 'grid',
+        gridTemplateColumns: '270px auto',
+        gridTemplateRows: '100px auto',
+        gridTemplateAreas:
+        `
+        "header header" 
+        "menu content"
+        `,
+});
