6e58f009d0578c12c4f9e9d05421cabe0b41a889
José Cássio <josecassio2013@gmail.com>
2023-04-16 14:15:54 -0300

    add profile preview

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,19 @@
 import Image from "next/image";
+import {AiOutlineDownCircle} from 'react-icons/ai'
 
 import { Container } from "./styles";
 
-import Logo from '../../assets/Logo2.png'
+import Logo from '../../assets/Logo2.png';
+import profilePicture from '../../assets/profile/Picture.png'
 
 export const Header = () => {
     return(
             <Container>
                 <Image src={Logo} alt="Logo escrito 'Vem questões'"/>
-                <div>
-                    Perfil
+                <div className="Profile">
+                    <Image src={profilePicture} alt="Foto de perfil do usuário"/>
+                    <span>Iago Nobre da Silva</span>
+                    <button><AiOutlineDownCircle/></button>
                 </div>
             </Container>
     );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -7,5 +7,49 @@ export const Container = styled('header', {
     display: "flex",
     alignItems: 'center',
     justifyContent: 'space-between',
+
+
+    '.Profile': {
+        display: "flex",
+        alignItems: "center",
+        gap: '10px',
+
+        padding: '2px 15px 2px 2px',
+        
+        background: "$white_1",
+
+        border: '1px solid $border',
+        borderRadius: '23px',
+
+
+        'img':{
+            width: '44px',
+            height: '44px',
+            borderRadius: '50%',
+        },
+
+        'p':{
+            fontWeight: 700,
+            fontSize: '16px',
+            lineHeight: '22px',
+        },
+
+        'button': {
+            background: 'none',
+            border: 'none',
+
+            display: 'grid',
+            placeContent: 'center',
+
+            fontSize: '24px',
+
+            transition: '0.3s',
+        },
+
+        'button:hover': {
+            cursor: 'pointer',
+        },
+
+    }
     
 })
