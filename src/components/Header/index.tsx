0ae9c9acc8b9f1ca3cbed447f532d8e497fc0e39
José Cássio <josecassio2013@gmail.com>
2023-04-16 16:20:13 -0300

    finish header component style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,51 @@
+import { useState } from 'react'
 import Image from "next/image";
-import {AiOutlineDownCircle} from 'react-icons/ai'
+import {AiOutlineDownCircle, AiOutlineUpCircle, AiOutlineLineChart, AiOutlineUser, AiOutlinePoweroff} from 'react-icons/ai'
 
-import { Container } from "./styles";
+import { Container, Profile, DropMenu, ProfileInfo } from "./styles";
 
 import Logo from '../../assets/Logo2.png';
 import profilePicture from '../../assets/profile/Picture.png'
 
 export const Header = () => {
+    const [showMenu, setShowMenu] = useState(false);
+
+    function handleChangeMenuState(){
+        setShowMenu(!showMenu);
+    }
+
     return(
             <Container>
                 <Image src={Logo} alt="Logo escrito 'Vem questões'"/>
-                <div className="Profile">
-                    <Image src={profilePicture} alt="Foto de perfil do usuário"/>
-                    <span>Iago Nobre da Silva</span>
-                    <button><AiOutlineDownCircle/></button>
-                </div>
+                <Profile>
+                    <ProfileInfo>
+                        <Image src={profilePicture} alt="Foto de perfil do usuário"/>
+                        <span>Iago Nobre da Silva</span>
+                        <button onClick={handleChangeMenuState}>
+                            {showMenu ? <AiOutlineUpCircle/> : <AiOutlineDownCircle/>}
+                        </button>
+                    </ProfileInfo>
+                    <DropMenu className={showMenu ? "show" : "hidden"}>
+                        <li>
+                            <button>
+                                <AiOutlineLineChart/>
+                                Perfil
+                            </button>
+                        </li>
+                        <li>
+                            <button>
+                                <AiOutlineUser/>
+                                Planos
+                            </button>
+                        </li>
+                        <li>
+                            <button>
+                                <AiOutlinePoweroff/>
+                                Sair
+                            </button>
+                        </li>
+                    </DropMenu>
+                </Profile>
             </Container>
     );
 } 
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,51 +5,100 @@ export const Container = styled('header', {
     padding: '23px 42px',
 
     display: "flex",
-    alignItems: 'center',
     justifyContent: 'space-between',
 
+    ".show": {
+        display: "flex"
+    },
+    ".hidden": {
+        display: "none"
+    }
+       
+});
 
-    '.Profile': {
+export const Profile = styled('div', {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+
+    background: "$white_1",
+    border: '1px solid $border',
+    borderRadius: '23px',
+
+    height: "max-content",
+
+    'button:hover':{
+        cursor: 'pointer',
+        color: "$blue_1"
+    },
+});
+
+export const ProfileInfo = styled("div", {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: '10px',
+
+    padding: '3px 15px 3px 2px',
+ 
+    background: "$white_1",
+
+    borderRadius: '23px',
+
+    'img':{
+        width: '44px',
+        height: '44px',
+        borderRadius: '50%',
+    },
+
+    'p':{
+        fontWeight: 700,
+        fontSize: '16px',
+        lineHeight: '22px',
+    },
+
+    'button': {
+        background: 'none',
+        border: 'none',
+
+        display: 'grid',
+        placeContent: 'center',
+
+        fontSize: '24px',
+
+        transition: '0.3s',
+    },
+});
+
+export const DropMenu = styled("ul", {
+    display: "flex",
+    flexDirection: "column",
+    gap: '10px',
+
+    width: '100%',
+
+    padding: '10px',
+
+    listStyle: "none",
+
+    transition: "0.5s",
+
+    'li button':{
+        background: 'none',
+        border: 'none',
+
         display: "flex",
         alignItems: "center",
         gap: '10px',
 
-        padding: '2px 15px 2px 2px',
-        
-        background: "$white_1",
+        fontWeight: 700,
+        fontSize: '16px',
+        lineHeight: '22px',
 
-        border: '1px solid $border',
-        borderRadius: '23px',
+        transition: "0.2s",
+    },
 
-
-        'img':{
-            width: '44px',
-            height: '44px',
-            borderRadius: '50%',
-        },
-
-        'p':{
-            fontWeight: 700,
-            fontSize: '16px',
-            lineHeight: '22px',
-        },
-
-        'button': {
-            background: 'none',
-            border: 'none',
-
-            display: 'grid',
-            placeContent: 'center',
-
-            fontSize: '24px',
-
-            transition: '0.3s',
-        },
-
-        'button:hover': {
-            cursor: 'pointer',
-        },
-
+    'li button svg':{
+        fontSize: '20px',
     }
-    
 })
