eedc71da7d2a87c03a000974f352a25e17ad3a31
José Cássio <josecassio2013@gmail.com>
2023-04-22 19:28:31 -0300

    create SearchInput Compoinent

---
+++ src/components/SearchInput/styles.ts
@@ -0,0 +1,31 @@
+import { styled } from "@/styles";
+
+export const Container = styled("div", {
+    width: '100%',
+    height: '56px',
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: '$white_2',
+    color: '$border',
+    border: '1px solid $border',
+
+    borderRadius: '23px',
+    fontSize: "20px",
+
+    padding: '12px',
+
+    "svg": {
+        marginLeft: "16px",
+    },
+
+    'input': {
+        width: '100%',
+        color: '$text',
+        background: 'transparent',
+        border: 0,
+        outline: 0,
+       ' &::placeholder': {
+            color: '$white_1',
+        }
+    }  
+})
