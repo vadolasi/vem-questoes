d8d603981c10066076610be344706189ab83d75e
José Cássio <josecassio2013@gmail.com>
2023-04-23 15:18:18 -0300

    create filter component

---
+++ src/components/Filter/styles.ts
@@ -0,0 +1,95 @@
+import {styled} from '@/styles'
+
+
+export const Container = styled("div", {
+    width: "930px",
+    padding: "16px",
+
+    margin: "0 auto",
+
+    background: "$white_1",
+
+    border: "1px solid $border",
+    borderRadius: "23px",
+
+    display: "flex",
+    flexDirection: "column",
+    gap: "15px",
+
+    '.inputs':{
+        display: 'flex',
+        alignItems: 'center',
+        gap: '20px',
+    },
+
+    '.Selects':{
+        display: 'grid',
+        gridTemplateColumns: "228px 228px 228px",
+        gridTemplateRows: "auto auto",
+        gap: "10px"
+    },
+
+    '.buttons': {
+        width: "100%",
+
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-end",
+        gap: "10px",
+
+        '.filter': {
+            color: "$blue_1",
+            border: "1px solid $blue_1",
+        },
+        '.reset': {
+            color: "$red",
+            border: "1px solid $red",
+        },
+    },
+
+});
+
+export const Fieldset = styled("fieldset", {
+    border: "none",
+    
+    "legend": {
+        fontFamily: 'Nunito',
+        fontWeight: 700,
+        fontSize: '14px',
+        lineHeight: '19px',
+    }
+});
+
+export const Button = styled("button", {
+    width: '131px',
+
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: "2px",
+
+    padding: "3px 10px",
+
+    fontWeight: 700,
+    fontSize: '14px',
+    lineHeight: '19px',
+
+    borderRadius: "33px",
+
+    background: "transparent",
+
+    cursor: 'pointer',
+
+    transition: "0.2s",
+
+    "&:hover":{
+        filter: "brightness(0.8)"
+    },
+
+    "svg": {
+        width: "20px",
+        height: "20px",
+    },
+
+
+})
