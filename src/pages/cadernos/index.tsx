27c0dd83a62e4f75e60633f022e4e90e0e4903aa
José Cássio <josecassio2013@gmail.com>
2023-04-26 15:49:15 -0300

    create notebook page

---
+++ src/pages/cadernos/index.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+
+import { Container, Content, Search } from './styles';
+
+import { Menu } from "@/components/Menu";
+import { Header } from "@/components/Header";
+import { SearchInput } from '@/components/SearchInput';
+import { Button } from '@/components/Button';
+import { DefaultSearchPage } from '@/components/DefaultSearchPage';
+
+
+import notebook from '@/assets/Notebook.png'
+
+export default function Home() {
+  return (
+    <Container>
+     <Header/>
+     <Menu/>
+     <Content>
+      <Search>
+        <SearchInput/>
+        <Button text='+ Criar Caderno'/>
+      </Search>
+        <DefaultSearchPage text='Crie um caderno para você!' picture={notebook} alt='Mulher escreven informações em um carderno'/>
+     </Content> 
+    </Container>
+  )
+}
---
+++ src/pages/cadernos/styles.ts
@@ -0,0 +1,35 @@
+import { styled } from '../../styles'
+
+export const Container = styled('div', {
+        width: "100vw",
+        height: "100vh",
+
+        display: 'grid',
+        gridTemplateColumns: '270px auto',
+        gridTemplateRows: '100px auto',
+        gridTemplateAreas:
+        `
+        "header header" 
+        "menu content"
+        `,
+});
+
+
+export const Content = styled('main', {
+        gridArea: 'content',
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+
+
+})
+export const Search = styled('div', {
+        display: "grid",
+        gridTemplateColumns: "500px 150px",
+        alignItems: "center",
+        width: "800px",
+        gap: "17px",
+
+        marginTop: "32px",
+        
+})
