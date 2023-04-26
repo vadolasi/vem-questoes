56f15e897337894b85edc2d993287be67f36b362
José Cássio <josecassio2013@gmail.com>
2023-04-26 15:37:50 -0300

    create xray page

---
+++ src/pages/raio-x/index.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+
+import { Container, Content, Search } from './styles';
+
+import { Menu } from "@/components/Menu";
+import { Header } from "@/components/Header";
+import { SearchInput } from '@/components/SearchInput';
+import { Select } from '@/components/Select';
+import { Button } from '@/components/Button';
+import { DefaultSearchPage } from '@/components/DefaultSearchPage';
+
+
+import raioX from '@/assets/raiox.png'
+
+export default function Home() {
+  return (
+    <Container>
+     <Header/>
+     <Menu/>
+     <Content>
+      <Search>
+        <SearchInput/>
+        <Select label='Banca' options={['nada']}/>
+        <Button text='Buscar'/>
+      </Search>
+        <DefaultSearchPage text='Raio-X completo da sua prova!' picture={raioX} alt='Mulher avaliando informações sobre algo'/>
+     </Content> 
+    </Container>
+  )
+}
