cf635bf9c814806762306450ff0f2f3cf99474b4
José Cássio <josecassio2013@gmail.com>
2023-04-28 10:46:14 -0300

    create DefaultBox page

---
+++ src/components/DefaultBoxQuestion/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Modal } from './styles';
+import Image from 'next/image';
+
+interface DefaultBoxQuestionProps {
+    h1?: string,
+    strong?: string,
+    picture?: any,
+    alt?: string,
+    children?: any,
+    className?: string,
+}
+
+export const DefaultBoxQuestion: React.FC<DefaultBoxQuestionProps> = ({className, h1, strong, picture, alt='', children}) => {
+    return (
+        <Modal className={className}>
+            <h1>
+                {h1} <br/>
+            <strong>{strong}</strong>
+            </h1>
+              <Image src={picture} alt={alt}/>   
+
+              {children}    
+        </Modal>
+    );
+};
+
