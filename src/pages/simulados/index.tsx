bcd979954fde6872d3c4521890533bdfe21e5cbf
José Cássio <josecassio2013@gmail.com>
2023-05-01 19:21:07 -0300

    add examcard

--- src/pages/simulados/index.tsx
+++ src/pages/simulados/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { useState } from 'react';
 
 import { Container, Content, Search } from './styles';
 
@@ -11,8 +12,24 @@ import { DefaultSearchPage } from '@/components/DefaultSearchPage';
 
 
 import simulado from '@/assets/Test.png'
+import { ExamBar } from '@/components/ExamBar';
 
 export default function Home() {
+  const [array, setArray] = useState<any[]>([
+    {title: 'caderno 1', descripition: "caderno feito agr", questions: ['1', '2', '3', 4, 5]}, 
+    {title: 'caderno 2', descripition: "caderno feito agr", questions: ['questao1', 'qua', 'qua']},
+  ]);
+
+  function handleRemoveExam(deleted: any){
+    const confirmDelete = confirm(`deseja excluir o caderno ${deleted.title}`);
+
+    if(!confirmDelete){
+      return
+    }
+
+    setArray(prevState => prevState.filter(notebook => notebook !== deleted))
+}
+
   return (
     <Container>
      <Header/>
@@ -23,7 +40,11 @@ export default function Home() {
         <Select label='Tipo' options={['Aleatório', 'Personalizado']}/>
         <Button text='Criar'/>
       </Search>
-        <DefaultSearchPage text='Você não possui simulados' picture={simulado} alt='Mulher resolvendo uma prova'/>
+        <DefaultSearchPage text='Você não possui simulados' picture={simulado} alt='Mulher resolvendo uma prova' content={array.length > 0}>
+        {array && array.map((exam, index) => (
+            <ExamBar key={index} title={exam.title} description={exam.descripition} questions={exam.questions} deleteClick={() => handleRemoveExam(exam)}/>
+            ))}
+        </DefaultSearchPage>
      </Content> 
     </Container>
   )
