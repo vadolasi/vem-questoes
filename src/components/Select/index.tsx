be3cf722d287db0709002b97cc2efbcd3c72d0fa
José Cássio <josecassio2013@gmail.com>
2023-05-03 10:30:00 -0300

    add value onchange

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -4,14 +4,17 @@ import { Container } from './styles';
 interface SelectProps {
     options: string[],
     label: string,
+    value?: string,
+    onChange?: any,
+
 }
 
-export const Select: React.FC<SelectProps> = ({options, label}) => {
+export const Select: React.FC<SelectProps> = ({options, label, value, onChange}) => {
     return (
         <Container>
         <label htmlFor={label}>{label}</label>
-        <select id={label}>
-            <option defaultValue=''> </option> 
+        <select id={label} value={value} onChange={onChange}>
+            <option defaultValue=''></option> 
 
             {options && options.map((option, index) => (
             <option key={String(index)} label={option} value={option}>{option}</option>
