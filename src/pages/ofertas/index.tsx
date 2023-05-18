786d130607b09bdafcad00af11bc4f1cda4563c0
José Cássio <josecassio2013@gmail.com>
2023-05-18 16:53:21 -0300

    add offer page

---
+++ src/pages/ofertas/index.tsx
@@ -0,0 +1,22 @@
+import { Container, Content } from '../../components/styles/ofertas';
+
+
+import { Menu } from "@/components/Menu";
+import { Header } from "@/components/Header";
+
+export default function Table() {
+  return (
+    <Container>
+     <Header/>
+     <Menu page=''/>
+     <Content>
+            <div></div>
+            <div></div>
+            <div></div>
+     </Content>
+    </Container>
+  )
+}
+
+
+
