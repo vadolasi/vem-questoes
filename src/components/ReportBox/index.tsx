d3901f257b6c70b7f5f3c1d12b7dfc32b3a44dee
José Cássio <josecassio2013@gmail.com>
2023-06-08 11:46:57 -0300

    add report mutation

--- src/components/ReportBox/index.tsx
+++ src/components/ReportBox/index.tsx
@@ -9,10 +9,34 @@ interface ReportBoxProps {
     question?: any
 }
 
+import { graphql } from '@/gql';
+import { useMutation, useQuery } from 'urql';
+ 
+const createReportMutation = graphql(/* GraphQL */ `
+  mutation CreateReport($title: String!, $content: String, $TicketType: String) {
+    addReport(
+      title: $title
+      content: $content
+      type: $TicketType
+    ) {
+      id
+      title
+      content
+      TicketType
+    }
+  }
+`);
+
 export const ReportBox: React.FC<ReportBoxProps> = ({show, question}) => {
     const [selectValue, setSelectValue] = useState('');
     const [title, setTitle] = useState('');
     const [motivo, setMotivo] = useState('');
+
+    const [, executeCreateReport] = useMutation(createReportMutation);
+
+    const addReport = async () => {
+        await executeCreateReport({ title: title, content: motivo, type: selectValue})
+      }
 
     return (
         <Container className={show ? 'show' : 'hidden'}>
@@ -33,7 +57,7 @@ export const ReportBox: React.FC<ReportBoxProps> = ({show, question}) => {
                 onChange={(e: any) => setMotivo(e.target.value)}
                 />
               </div>
-              <Button>Enviar</Button>
+              <Button onClick={() => addReport()}>Enviar</Button>
         </Container>
     );
 };
