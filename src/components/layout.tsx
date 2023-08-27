4ac3c6b5e4b6db69bbf7a4fb844caea4aaca2d95
José Cássio <josecassio2013@gmail.com>
2023-08-27 13:46:05 -0300

    add focus button

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,8 +3,10 @@ import { Container, Content } from "./styles/index";
 import { ContainerFocus } from "./styles/FocusContainer";
 import { Menu } from "@/components/Menu";
 import { Header } from "@/components/Header";
-import 'react-tooltip/dist/react-tooltip.css'
-import { Tooltip } from 'react-tooltip'
+import "react-tooltip/dist/react-tooltip.css";
+import { Tooltip } from "react-tooltip";
+
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
 interface Props {
   children: ReactNode;
@@ -14,47 +16,59 @@ interface Props {
 
 export default function Layout({ children, page, visible = false }: Props) {
   const [focus, setFocus] = useState(false);
+  const focusPages = ["banco-de-questoes", "cadernos", "simulados"];
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === "F11") {
-        setFocus(!focus)
+        setFocus(!focus);
       }
-    }
+    };
 
     if (visible) {
-      window.addEventListener("keydown", handleKeyDown)
+      window.addEventListener("keydown", handleKeyDown);
     }
 
     const handleFullScreen = () => {
       if (document.fullscreenElement) {
-        setFocus(true)
+        setFocus(true);
       } else {
-        setFocus(false)
+        setFocus(false);
       }
-    }
+    };
 
-    document.addEventListener("fullscreenchange", handleFullScreen)
+    document.addEventListener("fullscreenchange", handleFullScreen);
 
     const handleSize = () => {
-      if (window.innerWidth === screen.width && window.innerHeight === screen.height) {
-        setFocus(true)
+      if (
+        window.innerWidth === screen.width &&
+        window.innerHeight === screen.height
+      ) {
+        setFocus(true);
       } else {
-        setFocus(false)
+        setFocus(false);
       }
-    }
+    };
 
-    window.addEventListener("resize", handleSize)
+    window.addEventListener("resize", handleSize);
 
     return () => {
-      window.removeEventListener("keydown", handleKeyDown)
-      document.removeEventListener("fullscreenchange", handleFullScreen)
-      window.removeEventListener("resize", handleSize)
-    }
-  }, [focus])
+      window.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("fullscreenchange", handleFullScreen);
+      window.removeEventListener("resize", handleSize);
+    };
+  }, [focus]);
 
   return (
     <>
+      {focusPages.includes(page) && (
+        <button
+          className="fixed z-50 text-3xl rounded-full shadow-md text-accent bg-base-200 bottom-4 right-4"
+          onClick={() => setFocus(!focus)}
+        >
+          {focus ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
+        </button>
+      )}
       {focus ? (
         <ContainerFocus>
           <Tooltip id="tooltip" />
@@ -64,13 +78,13 @@ export default function Layout({ children, page, visible = false }: Props) {
         <div className="drawer lg:drawer-open">
           <Tooltip id="tooltip" />
           <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-          <div className="drawer-content flex flex-col">
+          <div className="flex flex-col drawer-content">
             <Container>
               <Header />
               <Content>{children}</Content>
             </Container>
           </div>
-          <div className="drawer-side z-50">
+          <div className="z-50 drawer-side">
             <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
             <Menu page={page} />
           </div>
