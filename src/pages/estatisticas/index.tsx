e241c85bdeb8c0c61732cec7e98465dd47f68514
José Cássio <josecassio2013@gmail.com>
2023-09-06 18:34:58 -0300

    add details per matter

--- src/pages/estatisticas/index.tsx
+++ src/pages/estatisticas/index.tsx
@@ -14,6 +14,30 @@ import { Chart } from "react-google-charts";
 
 import Layout from '@/components/layout';
 
+interface ProgressBarProps {
+  title: string;
+  totalQuestoes: number;
+  questoesAcertadas: number;
+}
+
+const ProgressBar: React.FC<ProgressBarProps> = ({ title, totalQuestoes, questoesAcertadas }) => {
+  const porcentagemAcertos = (questoesAcertadas / totalQuestoes) * 100;
+
+  return (
+    <div>
+      <h2 className='text-xl font-semibold'>{title}</h2>
+      <div className='w-full bg-blue-gray-300'>
+        <div className='pl-1 font-semibold text-white rounded-r-full bg-primary text-md' style={{ width: `${porcentagemAcertos}%` }}>
+          {porcentagemAcertos.toFixed(0)}%
+        </div>
+      </div>
+      <p>
+        Questões corretas: {questoesAcertadas} de {totalQuestoes}
+      </p>
+    </div>
+  );
+};
+
 
 const chartQuery = graphql(/* GraphQL */ `
   query Chart {
@@ -46,8 +70,9 @@ const [optionsBar, setOptionsBar] = useState({
   return (
     <Layout page="estatisticas">
       <Content>
-           <h1>Analise Geral</h1>
-           <div className='circularGraph'>
+           <h1 className='text-3xl font-bold'>Analise Geral</h1>
+           <div className='flex flex-col items-center justify-between w-full h-full gap-5 xl:flex-row'>
+           <div className='w-3/5 circularGraph'>
             <CircularProgressbar value={percentage} text={`${percentage.toFixed(2)}%`} className='circle'/>
             <span><strong>{data?.me?.totalQuestions}</strong> Questões Resolvidas</span>
             <span><strong>{data?.me?.totalCorrect}</strong> Acertos</span>
@@ -60,6 +85,17 @@ const [optionsBar, setOptionsBar] = useState({
      data={[['Data', 'Total', 'Certas'], ...(data?.relatorio.map((dia => [dia.date, dia.total, dia.totalCorrect])) || ["01/01/2023", 0, 0])]}
      options={optionsBar}
    />
+           </div>
+           <div className='w-[250px] border border-secondary rounded-md p-5 glex flex-col xl:w-[400px]'>
+            <h1 className='text-2xl font-bold'>Rendimento por matéria</h1>
+              <div>
+              <ProgressBar title='Respiração' totalQuestoes={10} questoesAcertadas={4}/>
+              <ProgressBar title='Trauma' totalQuestoes={10} questoesAcertadas={6}/>
+              <ProgressBar title='Neurofuncional' totalQuestoes={10} questoesAcertadas={2}/>
+              <ProgressBar title='Esporte' totalQuestoes={10} questoesAcertadas={1}/>
+              <ProgressBar title='Cardiovascular' totalQuestoes={10} questoesAcertadas={8}/>
+              </div>
+           </div>
            </div>
           </Content>
     </Layout>
