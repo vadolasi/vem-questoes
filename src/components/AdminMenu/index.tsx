195d3b56a4e3c5fb590b4878a42dfcebdbff1ba3
José Cássio <josecassio2013@gmail.com>
2023-09-24 09:57:40 -0300

    push list notifies

--- src/components/AdminMenu/index.tsx
+++ src/components/AdminMenu/index.tsx
@@ -1,8 +1,14 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import {AiOutlineHome, AiOutlineRead, AiOutlineExclamation, AiOutlineUserAdd, AiOutlineUser} from 'react-icons/ai'
-import { VscGraph } from 'react-icons/vsc'
-import { FC } from 'react';
+import Link from "next/link";
+import Image from "next/image";
+import {
+  AiOutlineHome,
+  AiOutlineRead,
+  AiOutlineExclamation,
+  AiOutlineUserAdd,
+  AiOutlineUser,
+} from "react-icons/ai";
+import { VscGraph } from "react-icons/vsc";
+import { FC } from "react";
 import Logo from "../../assets/logo.png";
 import { Container } from "./styles";
 
@@ -18,46 +24,72 @@ export const AdminMenu: FC<menuProps> = ({ page, invisible }) => {
         <div className="flex items-center h-20 pl-10">
           <div className="flex-1 hidden px-2 mx-2 md:block">
             <Link href="/">
-              <Image src={Logo} alt="Logo escrito 'Vem questões'" height={50} className="Logo" />
+              <Image
+                src={Logo}
+                alt="Logo escrito 'Vem questões'"
+                height={50}
+                className="Logo"
+              />
             </Link>
           </div>
         </div>
       </div>
-      <Container
-        className={`${invisible ? "hidden" : ""} border-r h-full`}
-      >
-            <li>
-                <Link href="/admin/questao" className={page == 'AddQuestao' ? 'page' : ''}>
-                        <AiOutlineRead/>
-                        Criar questão
-                </Link>
-            </li>
-            <li>
-                <Link href="/admin/notificacao" className={page == 'notificacao' ? 'page' : ''}>
-                    <AiOutlineExclamation/>
-                    Criar notificação
-                </Link>
-            </li>
-            <li>
-                <Link href="/admin/criarUsuario" className={page == 'userAdd' ? 'page' : ''}>
-                    <AiOutlineUserAdd/>
-                    Criar Usuários
-                </Link>
-            </li>
-            <li>
-                <Link href="/admin/listarUsuarios" className={page == 'userList' ? 'page' : ''}>
-                    <AiOutlineUser/>
-                    Listar Usuários
-                </Link>
-            </li>
-            <li>
-                <Link href="/admin/estatisticas" className={page == 'estatisticas' ? 'page' : ''}>
-                    <VscGraph/>
-                    Estatísticas
-                </Link>
-            </li>
+      <Container className={`${invisible ? "hidden" : ""} border-r h-full`}>
+        <li>
+          <Link
+            href="/admin/questao"
+            className={page == "AddQuestao" ? "page" : ""}
+          >
+            <AiOutlineRead />
+            Criar questão
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/admin/notificacao"
+            className={page == "notificacao" ? "page" : ""}
+          >
+            <AiOutlineExclamation />
+            Criar notificação
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/admin/listNotificacoes"
+            className={page == "ListNotificacoes" ? "page" : ""}
+          >
+            <AiOutlineExclamation />
+            Listar notificações
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/admin/criarUsuario"
+            className={page == "userAdd" ? "page" : ""}
+          >
+            <AiOutlineUserAdd />
+            Criar Usuários
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/admin/listarUsuarios"
+            className={page == "userList" ? "page" : ""}
+          >
+            <AiOutlineUser />
+            Listar Usuários
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/admin/estatisticas"
+            className={page == "estatisticas" ? "page" : ""}
+          >
+            <VscGraph />
+            Estatísticas
+          </Link>
+        </li>
       </Container>
     </div>
   );
 };
-
